07d23d8a2d758143c80720c440f5d42c325ae4b7
MoeeinAali <moeeeinaali@gmail.com>
2023-07-01 23:53:24 +0330

    Q1 not complete

---
+++ Q1.py
@@ -0,0 +1,21 @@
+import random
+
+# number of simulations
+n = 1000
+# number of flips in each simulation
+n_flips = 10
+# Keep track of "successes"
+num_success = 0
+cnt = 0
+buff1 = random.randint(0, 1)
+for i in range(2, n + 1):
+    if cnt >= n_flips - 1:
+        num_success = num_success + 1
+    buff2 = buff1
+    buff1 = random.randint(0, 1)
+    if buff1 == buff2:
+        cnt = cnt + 1
+    else:
+        cnt = 0
+print("num_success:   " + str(num_success))
+print("num_success per n:   " + str(num_success / n))
