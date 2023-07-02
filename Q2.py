d0e01c6f9d0e3aefaa88561fc7aa7b3261eb5a22
MoeeinAali <moeeeinaali@gmail.com>
2023-07-02 13:59:31 +0330

    Q2 not complete

--- Q2.py
+++ Q2.py
@@ -1,20 +1,24 @@
 import random
 
 # Number of simulation
-n = 100000
+n = 10000
 # Range
 m = 13
 # Keep track of "successes"
 num_success = 0
 
 
+
 def select_three_random_numbers():
     numbers = random.sample(range(1, m + 1), 3)
     return numbers
 
+
 for i in range(1, n + 1):
     random_numbers = select_three_random_numbers()
-    if abs(random_numbers[0] - random_numbers[1]) < random_numbers[2] and random_numbers[2] > random_numbers[0] + random_numbers[1]:
+    print(str(random_numbers[0])+" "+str(random_numbers[1])+" "+str(random_numbers[2]))
+    if abs(random_numbers[0] - random_numbers[1]) < random_numbers[2] and random_numbers[2] < (random_numbers[0] + random_numbers[1]):
+        print("Success ++")
         num_success = num_success + 1
 
 # Estimated probability
