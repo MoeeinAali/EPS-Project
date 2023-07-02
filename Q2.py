dd59130f7d4ce7f1093b1e0ebb63239a710674ab
MoeeinAali <moeeeinaali@gmail.com>
2023-07-02 16:31:27 +0330

    Q2 not complete - save outputs in a file

--- Q2.py
+++ Q2.py
@@ -1,27 +1,29 @@
 import random
 
 # Number of simulation
-n = 10000
+n = 100000
 # Range
 m = 13
 # Keep track of "successes"
 num_success = 0
 
-
-
+file = open("Q2_Output.txt", "w")
+file.truncate(0)
 def select_three_random_numbers():
     numbers = random.sample(range(1, m + 1), 3)
     return numbers
 
 for i in range(1, n + 1):
     random_numbers = select_three_random_numbers()
-    print(str(random_numbers[0])+" "+str(random_numbers[1])+" "+str(random_numbers[2]))
+    file.write(str(random_numbers[0])+" "+str(random_numbers[1])+" "+str(random_numbers[2])+"\n")
     if abs(random_numbers[0] - random_numbers[1]) < random_numbers[2] and random_numbers[2] < (random_numbers[0] + random_numbers[1]):
-        print("Success ++")
+        file.write("Success !"+"\n")
         num_success = num_success + 1
 
 # Estimated probability
-print("num_success:" + str(num_success))
+file.write("\n\nnum_success:" + str(num_success)+"\n")
+print("\n\nnum_success:" + str(num_success))
+file.write("Estimated probability:" + str(num_success / n))
 print("Estimated probability:" + str(num_success / n))
 
 # E[x] = 0.145
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-a = 5
-b = 6
-print(abs(a-b))
+# Open the file in write mode
+file = open("Q2_Output.txt", "w")
+
+# Write content into the file
+# file.write("Hello, World!")
+# file.write("This is an example.")
+
+# Close the file
+file.close()
