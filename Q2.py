723252d2f3b9e44837b1f0dcd1dff0a0241faceb
MoeeinAali <moeeeinaali@gmail.com>
2023-07-02 14:00:42 +0330

    Q2 not complete - edited

--- Q2.py
+++ Q2.py
@@ -13,7 +13,6 @@ def select_three_random_numbers():
     numbers = random.sample(range(1, m + 1), 3)
     return numbers
 
-
 for i in range(1, n + 1):
     random_numbers = select_three_random_numbers()
     print(str(random_numbers[0])+" "+str(random_numbers[1])+" "+str(random_numbers[2]))
