fe39541b88e6806fc3fb9cbaf2a79c403ff49cf9
MoeeinAali <moeeeinaali@gmail.com>
2023-07-03 13:07:52 +0330

    Q1&Q2 output files completed

--- Q1.py
+++ Q1.py
@@ -1,24 +1,32 @@
 import random
 
 # number of simulations
-n = 1000000
+n = 100000
 # number of flips in each simulation
 n_flips = 10
 # Keep track of "successes"
 num_success = 0
 cnt = 0
+
+file = open("Q1_Output.txt", "w")
+file.truncate(0)
+
 buff1 = random.randint(0, 1)
+file.write(str(buff1)+"\n")
 for i in range(2, n + 1):
     if cnt >= n_flips - 1:
         num_success = num_success + 1
+        file.write("Success "+str(num_success)+"!"+"\n")
     buff2 = buff1
     buff1 = random.randint(0, 1)
+    file.write(str(buff1)+"\n")
     if buff1 == buff2:
         cnt = cnt + 1
     else:
         cnt = 0
-print("num_success:" + str(num_success))
+print("\nnum_success:" + str(num_success))
 print("Estimated probability:" + str(num_success / n))
+
 
 
 # E[x] = 0.002
--- Q2.py
+++ Q2.py
@@ -17,8 +17,9 @@ for i in range(1, n + 1):
     random_numbers = select_three_random_numbers()
     file.write(str(random_numbers[0])+" "+str(random_numbers[1])+" "+str(random_numbers[2])+"\n")
     if abs(random_numbers[0] - random_numbers[1]) < random_numbers[2] and random_numbers[2] < (random_numbers[0] + random_numbers[1]):
-        file.write("Success !"+"\n")
         num_success = num_success + 1
+        file.write("Success "+str(num_success)+"!"+"\n")
+
 
 # Estimated probability
 file.write("\n\nnum_success:" + str(num_success)+"\n")
@@ -26,4 +27,4 @@ print("\n\nnum_success:" + str(num_success))
 file.write("Estimated probability:" + str(num_success / n))
 print("Estimated probability:" + str(num_success / n))
 
-# E[x] = 0.145
+# E[x] = 0.45
